d4f52e94dd5e926204b16e16c6c20acd100a1336
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-11-04 19:45:59 -0500

    Added .gitignore

--- server.js
+++ server.js
@@ -2,18 +2,31 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.port || 3001;
 
-app.use(express.static('public'));
+const htmlRoute = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoutes');
 
-app.get('/', (req, res) => {
-    res.send();
-})
+const port = process.env.port || 3001;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-app.listen(3001, () => {
-    console.log('Listening on Port 3001!');
-});
+app.use(express.static('public'));  // loads the root file, links the js file, and the css file
 
-app.get('/api/', (req, res) => {
-    
-})
+
+app.use(express.json());
+
+app.use(apiRoute);
+app.use(htmlRoute);
+
+// app.get('/', (req, res) => {
+//     res.send('Navigate to /index or /notes');
+// });
+
+// app.get('/index', (req, res) => 
+//     res.index(path.join(__dirname, 'public/index.html'))
+// );
+
+// app.get('/notes', (req, res) => 
+//     res.notes(path.join(__dirname, 'public/notes.html'))
+// );
+
+// app.get('/')
